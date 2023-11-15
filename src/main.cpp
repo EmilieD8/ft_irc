534017a04e84f21842fac3b9e91f82e0489cb9c1
mschaub <mars12161@gmail.com>
2023-11-15 11:30:32 +0100

    [irc] adapted the main

--- src/main.cpp
+++ src/main.cpp
@@ -18,5 +18,19 @@ int main(int argc, char **argv) {
     Binding bindsocket(AF_INET, SOCK_STREAM, 0, 6667, INADDR_ANY);
     std::cout << "Listening Socket..." << std::endl;
     Listening listensocket(AF_INET, SOCK_STREAM, 0, 6667, INADDR_ANY, 10);
-    std::cout << "SUCESS !" << std::endl; 
+    std::cout << "SUCESS !" << std::endl;
+
+    if (argc != 3) {
+        std::cerr << "Usage: ./ircserv  <port> <password>" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    try {
+        int port = std::stoi(argv[1]);
+        std::string password = argv[2];
+        /* Launch server */
+    }
+    catch (std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        exit(EXIT_FAILURE);
+    }
 }
